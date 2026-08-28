a96954c75d29ca5e18b93316663754edb7e73148
radiantiscollin90 <clam90@calpoly.edu>
2026-08-28 11:45:06 -0700

    Added decryptors for lab 1
    Added decryptors from lab assignment which reverses the encryption from the website.

--- Module/src/main/java/Main.java
+++ Module/src/main/java/Main.java
@@ -10,6 +10,79 @@ public class Main {
         return a + 17;
     }
 
+    //removes the '*' from the beginning and the end of a string
+    public static String decryptorA(String encryptedText) {
+        if(encryptedText.startsWith("*") && encryptedText.endsWith("*")) {
+            return encryptedText.substring(1, encryptedText.length() - 1);
+        } else {
+            return encryptedText;
+        }
+    }
+
+    //swaps the first and last index of a string
+    public static String decryptorB(String encryptedText) {
+        if(!encryptedText.isEmpty()) {
+            return encryptedText.charAt(encryptedText.length() - 1) +
+                    encryptedText.substring(1, encryptedText.length() - 1) +
+                    encryptedText.charAt(0);
+
+        }else {
+            return null;
+        }
+    }
+
+    //checks if the string ends with an "ay", removes it, then sends the last index back to the front.
+    public static String decryptorC(String encryptedText) {
+        String tempText;
+        if(encryptedText.endsWith("ay")) {
+            tempText = encryptedText.substring(0, encryptedText.length() - 2);
+            return tempText.charAt(tempText.length() - 1) +
+                    tempText.substring(0, tempText.length() - 1);
+
+        } else {
+            return null;
+        }
+    }
+
+    //swaps the middle and last index of a string
+    public static String decryptorD(String encryptedText) {
+        int targetIndex = encryptedText.length() / 2;
+        if(!encryptedText.isEmpty()) {
+            return encryptedText.substring(0, targetIndex) +
+                    encryptedText.charAt(encryptedText.length() - 1) +
+                    encryptedText.substring(targetIndex, encryptedText.length() - 1);
+        } else {
+            return null;
+        }
+    }
+
+    //unreverses the first half of the string, then brings the second half back to the front
+    public static String decryptorLA(String encryptedText) {
+        if(encryptedText.isEmpty()) return null;
+        int targetIndex = (encryptedText.length() / 2);
+        String firstHalf = encryptedText.substring(0,targetIndex + 1);
+        String secondHalf = encryptedText.substring(targetIndex + 1, encryptedText.length());
+        String newHalf = "";
+        //unreverse the first half
+        for(int i = firstHalf.length() - 1; i >= 0; i--) newHalf = newHalf + firstHalf.charAt(i);
+
+        return secondHalf + newHalf;
+    }
+
+
+    //for every vowel in the string, swap it for the vowel in the left of it in the vowel rhyme ( a e i o u)
+    public static String decryptorLB(String encryptedText) {
+        char[] vowels = {'a', 'e', 'i', 'o', 'u'};
+        if(encryptedText.isEmpty()) return null;
+
+        encryptedText = encryptedText.replace(vowels[0], 'A');
+        for(int i = 1; i < vowels.length; i++) {
+            encryptedText = encryptedText.replace(vowels[i], vowels[i - 1]);
+        }
+        encryptedText = encryptedText.replace('A', vowels[4]);
+        return encryptedText;
+    }
+
     static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
--- Module/src/test/java/MainTest.java
+++ Module/src/test/java/MainTest.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestTemplate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,5 +14,76 @@ class MainTest {
     void addSeventeen() {
         assertEquals(29, Main.addSeventeen(12));
     }
+
+    @Test
+    void decryptorA1() {
+        assertEquals("COLLIN", Main.decryptorA("*COLLIN*"));
+    }
+
+    @Test
+    void decryptorA2() {
+        assertEquals("*HELLO*", Main.decryptorA("**HELLO**"));
+    }
+
+    @Test
+    void decryptorA3() {
+        assertEquals("HE*LLO", Main.decryptorA("*HE*LLO*"));
+    }
+
+    @Test
+    void decryptorB1() {
+        assertEquals("collin", Main.decryptorB("nollic"));
+    }
+
+    @Test
+    void decryptorB2() {
+        assertEquals(null, Main.decryptorB(""));
+    }
+
+    @Test
+    void decryptorC1() {
+        assertEquals("collin", Main.decryptorC("ollincay"));
+    }
+
+    @Test
+    void decryptorC2() {
+        assertEquals(null, Main.decryptorC("normal"));
+    }
+
+    @Test
+    void decryptorD1() {
+        assertEquals("collinlam", Main.decryptorD("collnlami"));
+    }
+
+    @Test
+    void decryptorD2() {
+        assertEquals(null, Main.decryptorD(""));
+    }
+
+    @Test
+    void decryptorLA1() {
+        assertEquals("firefighter", Main.decryptorLA("rethgifiref"));
+    }
+
+    @Test
+    void decryptorLA2() {
+        assertEquals("collinlam", Main.decryptorLA("malnicoll"));
+    }
+
+    @Test
+    void decryptorLA3() {
+        assertEquals(null, Main.decryptorLA(""));
+    }
+
+    @Test
+    void decrypterLB1() {
+        assertEquals("collin", Main.decryptorLB("cullon"));
+    }
+
+    @Test
+    void decrypterLB2() {
+    assertEquals("a e i o u", Main.decryptorLB("e i o u a"));
+    }
+
 
 }
